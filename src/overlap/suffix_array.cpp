c24046b4a779a05a833b2d9077bc20ca3c99d4ef
Matija Osrecki <mosrecki@gmail.com>
2014-01-24 18:13:51 -0800

    Put sais file.

--- src/overlap/suffix_array.cpp
+++ src/overlap/suffix_array.cpp
@@ -1,8 +1,7 @@
 #include <stddef.h>
 
-#include <sais.h>
-
 #include "read.h"
+#include "sais.h"
 #include "suffix_array.h"
 
 
@@ -23,7 +22,7 @@ String* SaisSACA::BuildBWT(const ReadSet& reads, size_t depth) {
     bwt_size += reads[read_idx]->size();
   }
 
-  sa_int32_t* super_string = new sa_int32_t[bwt_size + 1];
+  int* super_string = new int[bwt_size + 1];
   uint32_t ss_pos = 0;
   for (uint32_t read_idx = 0; read_idx < num_strings; ++read_idx) {
     const Read* read = reads[read_idx];
@@ -34,15 +33,15 @@ String* SaisSACA::BuildBWT(const ReadSet& reads, size_t depth) {
   }
   super_string[bwt_size] = '\0';
 
-  sa_int32_t* bwt32 = new sa_int32_t[bwt_size];
+  int* bwt32 = new int[bwt_size];
 
-  if (sais_i32_bwt(super_string, bwt32, NULL, (sa_int32_t)bwt_size, depth) < 0) {
+  if (sais_int_bwt(super_string, bwt32, NULL, (int)bwt_size, depth) < 0) {
     return NULL;
   }
 
   uint8_t* bwt = new uint8_t[bwt_size + 1];
   for (uint32_t bwt_pos = 0; bwt_pos <= bwt_size; ++bwt_pos) {
-    sa_int32_t c = bwt32[bwt_pos];
+    int c = bwt32[bwt_pos];
     bwt[bwt_pos] = ((size_t)c < num_strings ? 0 : c - num_strings + 1);
   }
 
